2605db5d2227f5e44f80a48ecbc9dd552e824a2f
leaFleishman <l0548545588@gmail.com>
2026-09-15 17:52:37 +0300

    Add explicit database health check

--- SystemSalesTickets/Program.cs
+++ SystemSalesTickets/Program.cs
@@ -21,7 +21,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHealthChecks()
-    .AddDbContextCheck<DataContext>();
+    .AddDbContextCheck<DataContext>()
+    .AddCheck<DatabaseConnectionHealthCheck>("DatabaseConnection");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
